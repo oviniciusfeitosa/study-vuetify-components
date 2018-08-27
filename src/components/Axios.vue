97ef5450e59801fe82823e8c2a6b54a4a25985fd
vinnyfs89 <viniciusfesil@gmail.com>
2018-08-27 01:52:22 -0300

    [UPDATE] Iniciando tratamento de erros utilizando o `catch` do axios

--- src/components/Axios.vue
+++ src/components/Axios.vue
@@ -25,6 +25,7 @@ export default {
     axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(response => (this.info = response.data.bpi))
+      .catch(error => console.log(error))
   },
   filters: {
     currencydecimal (value) {
