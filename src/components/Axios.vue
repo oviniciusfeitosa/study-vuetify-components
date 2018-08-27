ba1fd953b3ff27b23cb45d9fa48c4048e71b03fb
vinnyfs89 <viniciusfesil@gmail.com>
2018-08-27 02:08:10 -0300

    [UPDATE] Adição de tratamento para quando a APi terminar de ser consultada utilizando o Axios.

--- src/components/Axios.vue
+++ src/components/Axios.vue
@@ -1,14 +1,40 @@
 <template>
-  <v-container fluid>
-    <h1>Bitcoin Price Index</h1>
-    <div v-for="currency in info" class="currency">
-      {{ currency.description }}
-      <span class="lighten">
-        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
-      </span>
-    </div>
-    
-  </v-container>
+  
+
+      <section v-if="errored">
+        <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+      </section>
+      
+      <section v-else>
+
+        <div v-if="loading">Loading...</div>
+
+        <v-card>
+          <v-container
+            fluid
+            grid-list-lg>
+            <v-layout row wrap>
+              <v-flex xs12>
+                <v-card color="blue-grey darken-2" class="white--text">
+                  <v-card-title primary-title>
+                    <div class="headline">Bitcoin Price Index</div>
+                  </v-card-title>
+                  <v-card-text>
+                    <div v-for="currency in info" class="currency">
+                      {{ currency.description }}
+                      <span class="lighten">
+                        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+                      </span>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+        
+      </section>
+
 </template>
 
 <script>
@@ -18,19 +44,25 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      info: null
+      info: null,
+      loading: true,
+      errored: false,
+    }
+  },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
     }
   },
   mounted () {
     axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then(response => (this.info = response.data.bpi))
-      .catch(error => console.log(error))
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
   },
-  filters: {
-    currencydecimal (value) {
-      return value.toFixed(2)
-    }
-  }
 }
 </script>
